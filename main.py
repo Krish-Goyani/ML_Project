3db01cf5c4ab9a1191b8a727815c5103dcd41428
Krish-Goyani <goyani@duck.com>
2024-01-28 15:23:55 +0530

    Data Ingestion,Validation and Transformation added

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from src.ML_Project.logging import logger
 
 from ML_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from ML_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from ML_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/ML_Project/config/configuration.py
+++ src/ML_Project/config/configuration.py
@@ -1,6 +1,6 @@
 from ML_Project.constants import *
 from ML_Project.utils.common import read_yaml,create_directories
-from ML_Project.entity.config_entity import DataIngestionConfig
+from ML_Project.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 
 # DataIngestion need this configurationmanager thats why it's created
 class ConfigurationManager:
@@ -29,3 +29,30 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config=self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config= DataValidationConfig(
+            root_dir=config.root_dir,
+            unzip_data_dir=config.unzip_data_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            all_schema=schema
+        )
+
+        return data_validation_config
+    
+    def get_data_transformation_config(self)->DataTransformationConfig:
+        config = self.config.data_transformation
+        create_directories([config.root_dir])
+
+        data_transformation_config=DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path
+        )
+
+        return data_transformation_config
