80df8a6d6c2eec560b782390b92ba92b66e7b92c
Krish-Goyani <goyani@duck.com>
2024-01-29 12:27:49 +0530

    model evalution added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src.ML_Project.logging import logger
 from ML_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ML_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from ML_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from ML_Project.pipeline.stage_04_model_trainer import ModelTrainingPipeline
+from ML_Project.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -35,3 +37,26 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Model Training Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = 'Model Evalution Stage'
+
+try:
+      logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+      model_evaluation = ModelEvaluationPipeline()
+      model_evaluation.main()
+      logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
---
+++ src/ML_Project/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,31 @@
+from ML_Project.config.configuration import ConfigurationManager
+from ML_Project.components.model_evaluation import ModelEvaluation
+from ML_Project.logging import logger
+from pathlib import Path
+
+
+
+STAGE_NAME = "Model Evalution Stage"
+
+class ModelEvaluationPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.save_results()
+
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
